85305f4ef422731ec4e5b6031c717dbb59c4ac72
Ali Akber <ali.akber@deliveryhero.com>
2024-11-03 13:54:29 +0100

    add ui model and tests

---
+++ lib/features/weather_forecast/presentation/model/weather_details_ui_model.dart
@@ -0,0 +1,72 @@
+import 'package:current_weather/features/weather_forecast/domain/entity/weather_details_entity.dart';
+import 'package:intl/intl.dart';
+
+const _fullNameFormat = "EEEE";
+const _shortNameFormat = "EEE";
+const _humidityUnit = "%";
+const _pressureUnit = "hPa";
+const _windSpeedUnit = "km/h";
+
+class WeatherDetailsUiModel {
+  final String dayNameFull;
+  final String dayNameShort;
+  final String tempMin;
+  final String tempMax;
+  final String pressure;
+  final String humidity;
+  final String partOfDay;
+  final String weatherCondition;
+  final String description;
+  final String icon;
+  final String windSpeed;
+
+  WeatherDetailsUiModel({
+    required this.dayNameFull,
+    required this.dayNameShort,
+    required this.tempMin,
+    required this.tempMax,
+    required this.pressure,
+    required this.humidity,
+    required this.partOfDay,
+    required this.weatherCondition,
+    required this.description,
+    required this.icon,
+    required this.windSpeed,
+  });
+
+  factory WeatherDetailsUiModel.fromWeatherDetailsEntity(
+    WeatherDetailsEntity entity,
+  ) {
+    return WeatherDetailsUiModel(
+      dayNameFull: _getDayNameFromTimeStamp(
+        entity.dateTime,
+        _fullNameFormat,
+      ),
+      dayNameShort: _getDayNameFromTimeStamp(
+        entity.dateTime,
+        _shortNameFormat,
+      ),
+      tempMin: entity.tempMin.toString(),
+      tempMax: entity.tempMax.toString(),
+      pressure: "${entity.pressure} $_pressureUnit",
+      humidity: "${entity.humidity} $_humidityUnit",
+      partOfDay: entity.partOfDay,
+      weatherCondition: entity.weather.isNotEmpty ? entity.weather[0].name : "",
+      description:
+          entity.weather.isNotEmpty ? entity.weather[0].description : "",
+      icon: entity.weather.isNotEmpty
+          ? _getWeatherImageUrl(entity.weather[0].icon)
+          : "",
+      windSpeed: "${entity.speed} $_windSpeedUnit",
+    );
+  }
+
+  static String _getDayNameFromTimeStamp(int timestamp, String format) {
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+    return DateFormat(format).format(date);
+  }
+
+  static String _getWeatherImageUrl(String imageIcon) {
+    return "https://openweathermap.org/img/wn/$imageIcon@2x.png";
+  }
+}
---
+++ test/features/weather_forecast/presentation/model/weather_details_ui_model_test.dart
@@ -0,0 +1,41 @@
+import 'package:current_weather/features/weather_forecast/domain/entity/weather_details_entity.dart';
+import 'package:current_weather/features/weather_forecast/presentation/model/weather_details_ui_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  test('WeatherDetailsUiModel fromWeatherDetailsEntity maps correctly', () {
+    const timestamp = 1661871600; // Unix timestamp for a specific date
+    final dateFullName = DateFormat("EEEE").format(DateTime.fromMillisecondsSinceEpoch(timestamp * 1000));
+    final dateShortName = DateFormat("EEE").format(DateTime.fromMillisecondsSinceEpoch(timestamp * 1000));
+    const weatherEntity = WeatherEntity(
+      name: 'Clear',
+      description: 'clear sky',
+      icon: '01d',
+    );
+    const weatherDetailsEntity = WeatherDetailsEntity(
+      dateTime: timestamp,
+      tempMin: 18.5,
+      tempMax: 25.3,
+      pressure: 1013,
+      humidity: 60,
+      partOfDay: 'day',
+      weather: [weatherEntity],
+      speed: 15.0,
+    );
+
+    final uiModel = WeatherDetailsUiModel.fromWeatherDetailsEntity(weatherDetailsEntity);
+
+    expect(uiModel.dayNameFull, dateFullName);
+    expect(uiModel.dayNameShort, dateShortName);
+    expect(uiModel.tempMin, '18.5');
+    expect(uiModel.tempMax, '25.3');
+    expect(uiModel.pressure, '1013 hPa');
+    expect(uiModel.humidity, '60 %');
+    expect(uiModel.partOfDay, 'day');
+    expect(uiModel.weatherCondition, 'Clear');
+    expect(uiModel.description, 'clear sky');
+    expect(uiModel.icon, 'https://openweathermap.org/img/wn/01d@2x.png');
+    expect(uiModel.windSpeed, '15.0 km/h');
+  });
+}
