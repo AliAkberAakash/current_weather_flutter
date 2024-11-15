39d54eca5dddcd8c22941c30f97503916e57d895
Ali Akber <ali.akber@deliveryhero.com>
2024-11-15 15:27:53 +0100

    add equtable to network response

--- lib/core/network/network_response.dart
+++ lib/core/network/network_response.dart
@@ -1,6 +1,11 @@
-class NetworkResponse {
+import 'package:equatable/equatable.dart';
+
+class NetworkResponse extends Equatable {
   final Map<String, dynamic> body;
   final Map<String, List<String>> headers;
 
-  NetworkResponse({required this.body, required this.headers});
+  const NetworkResponse({required this.body, required this.headers});
+
+  @override
+  List<Object?> get props => [body, headers];
 }
